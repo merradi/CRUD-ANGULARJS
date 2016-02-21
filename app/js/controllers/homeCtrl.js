d904460b7770d61d97ca4ce6bacbf28d491952d7
merradi <marouane.erradi@gmail.com>
2016-02-22 00:00:39 +0100

    Update homeCtrl.js

--- app/js/controllers/homeCtrl.js
+++ app/js/controllers/homeCtrl.js
@@ -1,24 +1,43 @@
 /**
  * Created by Training on 09/02/2016.
  */
-'use strict';
+ 'use strict';
 angular
     .module('controllers')
-    .controller('projectsHomeCtrl', function($scope, ProjectServices, $location, $routeParams) {
-        $scope.projects = ProjectServices.getProjects();
+    .controller('projectsHomeCtrl', function($scope, ProjectServices, $location, $routeParams, $log) {
+        //$scope.projects = ProjectServices.getProjects();
+        ProjectServices
+            .getProjects()
+            .then(function(projects){
+                //response;
+                $log.debug('Projects => ', projects);
+                $scope.projects = projects;
+            });
 
         $scope.edit = function (project) {
-            $location.path("/editproject/" + project.id);
+            $location.path("/editproject/" + project._id);
         };
 
-        $scope.remove = function(project){
-            ProjectServices.remove(project.id);
+        $scope.deleteProject = function(project){
+            ProjectServices.remove(project);
+
+            ProjectServices
+                .getProjects()
+                .then(function(projects){
+                    //response;
+                    $log.debug('Projects => ', projects);
+                    $scope.projects = projects;
+                });
         };
 
     })
 
 
-   ;
+   .controller('ProjectDetailsCtrl', function($scope, $routeParams, ProjectServices){
+    $scope.selected = ProjectServices.getProjectById($routeParams.id);
+
+    });
+
 
 
 
